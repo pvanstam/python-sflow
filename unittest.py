3fc55c2b37c462ac86c5fbc29ab5b23b65277826
Pim van Stam <pim@keysolutions.nl>
2016-06-27 14:58:47 +0200

    unittest: add repr_flow to use repr() function of datagram to represent the sFlow data

--- unittest.py
+++ unittest.py
@@ -14,6 +14,9 @@ def show_num_records(s_records):
         elif sample.sample_type == 2:
             print("    CountersSample: %d records" % sample.num_counter_records)
 
+def repr_flow(flow_datagram):
+    return repr(flow_datagram)
+
 
 if __name__ == '__main__':
     listen_addr = ("0.0.0.0", 5700)
@@ -24,7 +27,11 @@ if __name__ == '__main__':
         data, addr = sock.recvfrom(65535)
         flow_data = sflow.Datagram()
         flow_data.unpack(addr, data)
-#        sys.stdout.write(repr(flow_data))
-#        sys.stdout.flush()
-        print("Flow: %d (%d samples)" % (flow_data.sequence_number, flow_data.num_samples))
-        show_num_records(flow_data.sample_records)
+
+        # Test 1
+        # print("Flow: %d (%d samples)" % (flow_data.sequence_number, flow_data.num_samples))
+        #show_num_records(flow_data.sample_records)
+
+        # Test 2
+        print(repr_flow)
+        
